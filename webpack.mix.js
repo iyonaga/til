96b94b5652579df0eb44f780c853a46150524852
iyonaga <yiyonaga@gmail.com>
2017-09-03 19:18:57 +0900

    Add build settings

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+  //logLevel: 'silent',
+  ghostMode: false,
+  online: false,
+  files: ["public/**/*"],
+  open: false,
+  port: 8080,
+  proxy: '',
+  ui: false,
+  server: {
+    baseDir: "./public/",
+  },
+});
