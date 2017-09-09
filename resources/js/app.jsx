04db712501a0897016c3cf5aef4a0be1eb7cd98b
iyonaga <yiyonaga@gmail.com>
2017-09-09 22:08:20 +0900

    Added

---
+++ resources/js/app.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header.jsx';
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Header/>
+        <div className="hello-message">
+          Hello {this.props.name}
+        </div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <HelloMessage name="John"/>,
+  document.getElementById('hello')
+);
+
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+const user = {
+  firstName: 'yusuke',
+  lastName: 'iyonaga'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+
+
+ReactDOM.render(
+  element,
+  document.getElementById('hello-user')
+)
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('tick')
+)
