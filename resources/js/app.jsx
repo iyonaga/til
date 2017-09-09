2efee5d86110a558138eb128ad69ad9754c2821c
iyonaga <yiyonaga@gmail.com>
2017-09-09 22:52:16 +0900

    Handling Events

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -81,3 +81,33 @@ ReactDOM.render(
   <Clock />,
   document.getElementById('tick')
 )
+
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('toggle-button')
+)
