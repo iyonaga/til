35e8bf34221912dd602c7ecc1a99b31ed129dcb6
iyonaga <yiyonaga@gmail.com>
2017-09-10 14:33:37 +0900

    Lists and Keys

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -198,3 +198,33 @@ ReactDOM.render(
   <LoginControl />,
   document.getElementById('login-control')
 )
+
+function ListItem(props) {
+  return (
+    <li>{props.value}</li>
+  );
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  // const listItems = numbers.map((number) =>
+  //   <ListItem key={number.toString()} value={number} />
+  // );
+  // return (
+  //   <ul>{listItems}</ul>
+  // );
+
+  return (
+    <ul>
+      {numbers.map((number) =>
+        <ListItem key={number.toString()} value={number} />
+      )}
+    </ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('list')
+)
