887e8fa7376ef1ec8ed10669ee5b173fe76cc1da
iyonaga <yiyonaga@gmail.com>
2018-01-26 00:09:45 +0900

    toao-app-with-redux: Use redux-actions

--- react/todo-app-with-redux/src/redux/modules/todoApp.js
+++ react/todo-app-with-redux/src/redux/modules/todoApp.js
@@ -1,23 +1,22 @@
-export function addTodo(title) {
-  return {
-    type: 'ADD_TODO',
-    title
-  }
-}
+import { createAction, createActions, handleActions } from 'redux-actions';
 
-export function updateFilter(filter) {
-  return {
-    type: 'UPDATE_FILTER',
-    filter
-  }
-}
+const ADD_TODO      = 'ADD_TODO';
+const UPDATE_FILTER = 'UPDATE_FILTER';
+const UPDATE_STATUS = 'UPDATE_STATUS';
 
-export function updateStatus(idx) {
-  return {
-    type: 'UPDATE_STATUS',
-    idx
-  }
-}
+// export const addTodo = createAction(ADD_TODO, title => ({ title }));
+// export const updateFilter = createAction(UPDATE_FILTER, filter => ({ filter }));
+// export const updateStatus = createAction(UPDATE_STATUS, idx => ({ idx }));
+
+export const {
+  addTodo,
+  updateFilter,
+  updateStatus
+} = createActions({
+  ADD_TODO: title => ({ title}),
+  UPDATE_FILTER: filter => ({ filter }),
+  UPDATE_STATUS: idx => ({ idx })
+});
 
 const initialState = {
   todos: [
@@ -31,40 +30,40 @@ const initialState = {
     }
   ],
   filter: 'all'
-}
+};
 
-export default function reducer(state = initialState, action) {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            title: action.title,
-            status: 'active'
-          }
-        ]
-      })
+const reducer = handleActions({
+  ADD_TODO: (state, action) => {
+    return Object.assign({}, state, {
+      todos: [
+        ...state.todos,
+        {
+          title: action.payload.title,
+          status: 'active'
+        }
+      ]
+    });
+  },
 
-    case 'UPDATE_FILTER':
-      return Object.assign({}, state, {
-        filter: action.filter
-      });
+  UPDATE_FILTER: (state, action) => {
+    return Object.assign({}, state, {
+      filter: action.payload.filter
+    });
+  },
 
-    case 'UPDATE_STATUS':
-      const status = state.todos[action.idx].status === 'active' ? 'completed': 'active'
+  UPDATE_STATUS: (state, action) => {
+    const status = state.todos[action.payload.idx].status === 'active' ? 'completed': 'active'
 
-      return Object.assign({} , state, {
-        todos: [
-          ...state.todos.slice(0, action.idx),
-          Object.assign({}, state.todos[action.idx], {
-            status
-          }),
-          ...state.todos.slice(action.idx + 1)
-        ]
-      });
-
-    default:
-      return state
+    return Object.assign({} , state, {
+      todos: [
+        ...state.todos.slice(0, action.payload.idx),
+        Object.assign({}, state.todos[action.payload.idx], {
+          status
+        }),
+        ...state.todos.slice(action.payload.idx + 1)
+      ]
+    });
   }
-}
+}, initialState);
+
+export default reducer;
