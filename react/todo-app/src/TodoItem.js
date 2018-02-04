51cf309d663ce3960185ec91c8230f67fd995962
iyonaga <yiyonaga@gmail.com>
2018-02-04 22:28:37 +0900

    todo-app: Split component

--- react/todo-app/src/TodoItem.js
+++ react/todo-app/src/TodoItem.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+
+export default class TodoItem extends Component {
+  render() {
+    const { todo, i, isCompleted, handleChange } = this.props;
+    return (
+      <li>
+        <input type="checkbox" onChange={handleChange} value={i} checked={isCompleted} />
+          { isCompleted ? <s>{todo.title}</s> : todo.title }
+      </li>
+    )
+  }
+}
