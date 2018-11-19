e0f3f1517f7421b859ab1be6058e63bf1ec09956
iyonaga <yiyonaga@gmail.com>
2018-11-19 16:43:28 +0900

    Android: add MyAlarmClock app

---
+++ Android/MyAlarmClock/app/src/main/java/com/example/iyonaga/myalarmclock/AlarmBroadcastReceiver.kt
@@ -0,0 +1,15 @@
+package com.example.iyonaga.myalarmclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+
+class AlarmBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        context?.run {
+            startActivity(intentFor<MainActivity>("onReceive" to true).newTask())
+        }
+    }
+}
