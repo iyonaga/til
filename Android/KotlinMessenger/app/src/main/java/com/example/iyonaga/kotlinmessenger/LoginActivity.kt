647e17b814391a1302a3b27afcf8af91a0d84241
iyonaga <yiyonaga@gmail.com>
2018-11-22 23:30:54 +0900

    KotlinMessanger: Create Register and Login UI

---
+++ Android/KotlinMessenger/app/src/main/java/com/example/iyonaga/kotlinmessenger/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.iyonaga.kotlinmessenger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+        }
+
+        back_to_registar_textview.setOnClickListener {
+            finish()
+        }
+    }
+}
